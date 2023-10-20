6dd9abdf92b2175ebfbb472b0f29558cea1570df
Josh <joshuamares180@gmail.com>
2023-10-19 20:02:51 -0700

    finished search functionality

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 let currentAnimeJSON;
-let currentAnimeTitleList = [];
+let currentAnimeTitlesList = [];
+let allAnimeTitlesList = [];
 
 async function newGame(){
     const topJSON = await getTopAnime();
-    console.log(topJSON)
+    console.log(topJSON);
+    getAllTitles(topJSON);
     let randomNum = Math.floor(Math.random() * topJSON.length);
     console.log(randomNum);
     currentAnimeJSON = topJSON[randomNum];
     //console.log(animeJSON);
     //const animeJSON = await getAnimeJSON();
     renderImage(currentAnimeJSON);
-    getAllTitles(currentAnimeJSON);
+    getCorrectTitles(currentAnimeJSON);
 }
 
-function getAllTitles(animeJSON){
-    currentAnimeTitleList.length = 0;
-    for(let i=0; i < animeJSON.titles.length; i++){
-        currentAnimeTitleList.push(animeJSON.titles[i].title);
+function getAllTitles(animeJSONList){
+    allAnimeTitlesList.length = 0;
+    for(let i = 0; i < animeJSONList.length; i++){
+        for(let j = 0; j < animeJSONList[i].titles.length; j++){
+            allAnimeTitlesList.push(animeJSONList[i].titles[j].title);
+        }
     }
-    console.log(currentAnimeTitleList);
+    console.log(allAnimeTitlesList);
+}
+
+function getCorrectTitles(animeJSON){
+    currentAnimeTitlesList.length = 0;
+    for(let i=0; i < animeJSON.titles.length; i++){
+        currentAnimeTitlesList.push(animeJSON.titles[i].title);
+    }
+    console.log(currentAnimeTitlesList);
 }
 
 async function getTopAnime(){
@@ -33,14 +45,14 @@ async function getAnimeJSON(){
     return temp;
 }
 
-async function renderImage(animeJSON){
+function renderImage(animeJSON){
     let imageLink = animeJSON.images.jpg.large_image_url;
 
-    const imageContainer = document.getElementById("image");
+    const imageContainer = document.getElementById("image-container");
     //let currImageHTML = imageContainer.innerHTML;
     let newImageHTML  = (
         `<figure>
-                <img src="${imageLink}" />
+                <img src="${imageLink}" id="image"/>
         </figure>`
     );
     imageContainer.innerHTML = newImageHTML;
@@ -50,12 +62,41 @@ let guesses = [];
 const guessesList = document.getElementById("guesses");
 const guessInput = document.getElementById("text-input");
 const guessButton = document.getElementById("submit");
+const searchResultsList = document.getElementById("search-results");
+
+guessInput.onkeyup = function(){
+    let results = [];
+    let guess = guessInput.value;
+    if(guess.length){
+        results = allAnimeTitlesList.filter((title) =>{
+            return title.toLowerCase().includes(guess.toLowerCase());
+        });
+        console.log(results);
+    }
+    renderAutoComplete(results);
+}
+
+function renderAutoComplete(results){
+    const content = results.map((result) => {
+        return "<li onclick=selectInput(this)>" + result + "</li>";
+    });
+    searchResultsList.innerHTML = "<ul>" + content.join('') + "</ul>"; 
+}
+
+function selectInput(listItem){
+    guessInput.value = listItem.innerHTML;
+    searchResultsList.innerHTML = "";
+}
 
 function submitGuess(e){
     e.preventDefault();
     let currentGuess = guessInput.value;
-    if(currentAnimeTitleList.includes(currentGuess)){
+    if(currentAnimeTitlesList.includes(currentGuess)){
         console.log("Correct Guess!!!");
+        guesses.length = 0;
+        guessesList.innerHTML = "";
+        renderGuesses();
+        guessInput.value = "";
     }else{
         console.log(guessInput.value);
         guesses.push(guessInput.value);
@@ -86,6 +127,4 @@ function renderGuesses(){
     });
 }
 
-
 newGame();
-renderGuesses();
