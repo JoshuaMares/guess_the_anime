316de0d4eaa7da037e062678e7d74a04ac568d9c
Josh <joshuamares180@gmail.com>
2023-10-22 23:46:29 -0700

    mostly finished, just add popup to info and mayb adjust difficulty

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
+let topJSON;
 let currentAnimeJSON;
 let currentAnimeTitlesList = [];
 let allAnimeTitlesList = [];
-let stage = 0;
-let difficulty = 1;
+let stage = 1;
+let difficulty = 0;
+let winCount = 5;
+let currentLeft;
+let currentTop;
 
-async function newRound(){
+function shuffle(array) {
+    //fisher yates shuffle
+    let currentIndex = array.length,  randomIndex;
 
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
 
-
-async function newGame(){
-    const topJSON = await getTopAnime();
-    console.log(topJSON);
-    getAllTitles(topJSON);
-    let randomNum = Math.floor(Math.random() * topJSON.length);
-    console.log(randomNum);
-    currentAnimeJSON = topJSON[randomNum];
-    //console.log(animeJSON);
-    //const animeJSON = await getAnimeJSON();
+async function newRound(){
+    if(winCount == 5){
+        difficulty++;
+        winCount = 0;
+        topJSON = shuffle(await getTopAnime());
+        getAllTitles(topJSON);
+        console.log("new difficulty");
+    }
+    currentAnimeJSON = topJSON.shift();
     renderImage(currentAnimeJSON);
     getCorrectTitles(currentAnimeJSON);
 }
@@ -41,7 +59,7 @@ function getCorrectTitles(animeJSON){
 }
 
 async function getTopAnime(){
-    const temp = await fetch(`https://api.jikan.moe/v4/top/anime`).then(res => res.json());
+    const temp = await fetch(`https://api.jikan.moe/v4/top/anime?page=${difficulty}&filter=bypopularity`).then(res => res.json());
     console.log(temp.data.length);
     return temp.data;
 }
@@ -63,6 +81,47 @@ function renderImage(animeJSON){
         </figure>`
     );
     imageContainer.innerHTML = newImageHTML;
+    
+    //random starting position
+    setWindow();
+}
+
+function setWindow(){
+    let imageCSS = document.getElementById("image").style;
+    imageCSS.width = `${6 - stage}00%`;
+    imageCSS.height = `${6 - stage}00%`;
+    if(stage == 1){
+        currentLeft = Math.floor(Math.random() * (266 - 266/5));
+        console.log("current left: " + currentLeft);
+        currentTop = Math.floor(Math.random() * (400 - 400/5));
+        console.log("current top: " + currentTop);
+        //since width increases image size we need to adjust
+        imageCSS.left = `-${currentLeft * (6 - stage)}px`;
+        imageCSS.top = `-${currentTop * (6 - stage)}px`;
+        console.log("current left: " + currentLeft);
+        console.log("current top: " + currentTop);
+    }else if(stage == 5){
+        imageCSS.left = '';
+        imageCSS.top = '';
+    }
+    else{
+        let leftMax = currentLeft - ((266/(6-stage)) - (266/(6-(stage-1))));
+        let leftMin = currentLeft;
+        currentLeft = Math.min(266 - 266/(6-stage) , getRandomInt(leftMin, leftMax));
+        let topMax = currentTop - ((400/(6-stage)) - (400/(6-(stage-1))));
+        let topMin = currentTop;
+        currentTop = Math.min(400 - 400/(6-stage) , getRandomInt(topMin, topMax));
+        imageCSS.left = `-${currentLeft * (6 - stage)}px`;
+        imageCSS.top = `-${currentTop * (6 - stage)}px`;
+        console.log("current left: " + currentLeft);
+        console.log("current top: " + currentTop);
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
 let guesses = [];
@@ -71,7 +130,9 @@ const guessInput = document.getElementById("text-input");
 const guessButton = document.getElementById("submit");
 const searchResultsList = document.getElementById("search-results");
 
-guessInput.onkeyup = function(){
+guessInput.onkeyup = liveSearch;
+
+function liveSearch(){
     let results = [];
     let guess = guessInput.value;
     if(guess.length){
@@ -105,38 +166,68 @@ function submitGuess(e){
         /*guesses.length = 0;
         guessesList.innerHTML = "";
         renderGuesses();*/
-        renderNewGuess(currentGuess, true);
-        renderEndScreen();
+        stage = 5;
+        winCount++;
+        renderImage(currentAnimeJSON);
+        renderNewGuess(currentGuess, "win");
+        renderEndScreen("win");
         guessInput.value = "";
+    }else if(stage == 5){
+        renderImage(currentAnimeJSON);
+        renderNewGuess(currentGuess, "loss");
+        renderEndScreen("loss");
     }else{
         console.log(guessInput.value);
         /*guesses.push(guessInput.value);
         guessesList.innerHTML = "";
         renderGuesses();*/
-        renderNewGuess(currentGuess, false);
+        stage++;
+        renderImage(currentAnimeJSON);
+        renderNewGuess(currentGuess, "guess");
         guessInput.value = "";
     }
 }
 
-function renderEndScreen(){
+function renderEndScreen(outcome){
     //hide search bar and add new game button
-    document.getElementById("enter-guess").innerHTML = "";
-    document.getElementById("field").innerHTML += 
+    let enterGuessCSS = document.getElementById("enter-guess").style;
+    enterGuessCSS.display = 'none';
+    //enterGuessCSS.position = 'absolute';
+    //enterGuessCSS.visibility = 'hidden';
+    document.getElementById("end-screen").innerHTML = 
     `
     <section id="anime-description-container"><p>${currentAnimeJSON.synopsis}</p></section>
     <section id="new-game-button-container">
-        <button type="button" id="new-game-button">NextGame</button>
-    </section>`;
+        <button type="button" id="new-game-button" class="${outcome == "win" ? "win-button" : "loss-button"}">Next Game</button>
+    </section>
+    `;
+    document.getElementById("new-game-button").addEventListener("click", ()=> {
+        stage = 1;
+        enterGuessCSS.display = '';
+        //enterGuessCSS.position = '';
+        //enterGuessCSS.visibility = '';
+        document.getElementById("guesses").innerHTML = ``;
+        document.getElementById("end-screen").innerHTML = ``;
+        /*document.getElementById("enter-guess").innerHTML = `
+            <form action="">
+                <input type="text" id="text-input" placeholder="Search..." autocomplete="off">
+                <button type="input" id="submit">Submit</button>
+            </form>
+            <section id="search-results"></section>`;
+        guessButton.addEventListener("click", submitGuess);
+        guessInput.onkeyup = liveSearch();*/
+        newRound();
+    });
 }
 
-function renderNewGuess(guess, correctGuessBool){
-    if(correctGuessBool){
+function renderNewGuess(guess, guessClass){
+    if(guessClass == "win"){
         guessesList.innerHTML = (
             `<div class="guessItem winningGuess">
             <a href="${currentAnimeJSON.url}" target="_blank"><p>${guess} <i class="fa-solid fa-up-right-from-square"></i></p></a>
             </div>`
         );
-    }else{
+    }else if(guessClass == "guess"){
         guessesList.innerHTML += (
             `<div class="guessItem">
                 <p>${guess}</p>
@@ -146,6 +237,12 @@ function renderNewGuess(guess, correctGuessBool){
                     <i class="fa-brands fa-github github-icon"></i>
                 </div>
                 -->
+            </div>`
+        );
+    }else if(guessClass == "loss"){
+        guessesList.innerHTML = (
+            `<div class="guessItem losingGuess">
+            <a href="${currentAnimeJSON.url}" target="_blank"><p>${currentAnimeJSON.titles[0].title} <i class="fa-solid fa-up-right-from-square"></i></p></a>
             </div>`
         );
     }
@@ -174,4 +271,4 @@ function renderGuesses(){
 */
 
 
-newGame();
+newRound();
